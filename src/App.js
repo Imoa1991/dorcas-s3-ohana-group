efa49da48f9af373a63ab9fb4504714779f6b52d
Imoa1991 <ordonez.im@gmail.com>
2018-07-27 10:58:55 +0200

    pages incorporado

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Page from './components/Page'
 class App extends Component {
 constructor(props){
   super(props);
-    // this.estado = ':-)';
 }
   render() {
     return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,6 @@ class App extends React.Component {
 			<main className="main__2">
 					<Form />
           <Visor />
-				{/* <section className="card">{this.props.cariDos}</section> */}
 			</main>
 			);
 	}
