31151e1237b2463c2f7c32727b3726a0e25845f7
Imoa1991 <ordonez.im@gmail.com>
2018-07-31 13:57:42 +0200

    empezado api

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ class App extends Component {
 
   constructor(props){
     super(props);
-
     this.temp = this.temp.bind(this);
-
-
     this.state = {
       copyright:'Awesome profile-cards @ 2018',
       adalab:'http://adalab.es/',
       titleD: 'Diseña',
       tituloRellena: 'Rellena',
-      fontTypes: [ 'Ubuntu', 'Comic Sans', 'Montserrat' ]
+      fontTypes: [ 'Ubuntu', 'Comic Sans', 'Montserrat' ],
+      habilidades: ''
+    };
+    callAbilitiesAPI = () => {
+      const url = 'https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json';
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          this.abilitiesList = json.skills;
+      });
     };
   }
 
@@ -34,6 +40,12 @@ class App extends Component {
       <React.Fragment>
       <button className="botonTemporal" onClick={this.temp}>CLICAR AQUÍ PARA EVENTO TEMPORAL</button>
       <Page tituloRellena={this.state.tituloRellena} titleD={this.state.titleD} footerCopy={this.state.copyright} footerUrl={this.state.adalab} fontTypes={this.state.fontTypes}/>
+      <label htmlFor="habilidaes">Habilidades </label>
+        <select name="habilidades" id="habilidades" value={this.state.habilidades} onChange={this.filmGenero}>
+          <option value="html">html</option>
+          <option value="scss">scss</option>
+          <option value="js">js</option>
+        </select>
       </React.Fragment>
     );
   }
