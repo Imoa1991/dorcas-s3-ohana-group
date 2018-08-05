e98d32835ba36261fec4a6d0707e3880fe14f8c5
lauradml <laura.domingo.82@gmail.com>
2018-08-05 12:07:09 +0200

    resuelta App.js

--- src/App.js
+++ src/App.js
@@ -17,13 +17,23 @@ class App extends Component {
       name: "Nombre y Apellido",
       job: "Front End Developer",
       palette:1,
-      tipography: 1
+      tipography: 1,
+      email:'',
+      phone:'',
+      linkedin:'',
+      github:''
+
     };
       this.callAbilitiesAPI();
       this.changeName = this.changeName.bind(this);
       this.changeJob = this.changeJob.bind(this);
       this.changePalette = this.changePalette.bind(this);
       this.changeTipography = this.changeTipography.bind(this);
+      this.changeEmail = this.changeEmail.bind(this);
+      this.changePhone = this.changePhone.bind(this);
+      this.changeLinkedin = this.changeLinkedin.bind(this);
+      this.changeGithub = this.changeGithub.bind(this);
+
   }
 
 changeName(e){
@@ -47,6 +57,29 @@ changeTipography(e){
     tipography: e.currentTarget.value
   })
 };
+changeEmail(e){
+  this.setState({
+    email: e.currentTarget.value
+  })
+};
+changePhone(e){
+  this.setState({
+    phone: e.currentTarget.value
+  })
+};
+changeLinkedin(e){
+  this.setState({
+    linkedin: e.currentTarget.value
+  })
+};
+changeGithub(e){
+  this.setState({
+    github: e.currentTarget.value
+  })
+};
+
+
+
   callAbilitiesAPI = () => {
     const url = 'https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json';
     fetch(url)
@@ -71,7 +104,7 @@ changeTipography(e){
     return (
       <React.Fragment>
         <button className="botonTemporal" onClick={this.temp}>CLICAR AQUÍ PARA EVENTO TEMPORAL</button>
-        <Page changeName={this.changeName} name={this.state.name}  changeJob={this.changeJob} job={this.state.job}  tituloRellena={this.state.tituloRellena} titleD={this.state.titleD} footerCopy={this.state.copyright} footerUrl={this.state.adalab} fontTypes={this.state.fontTypes} skills={this.state.habilidades} palette={this.state.palette} changePalette={this.changePalette} tipography={this.state.tipography} changeTipography={this.changeTipography} />
+        <Page changeName={this.changeName} name={this.state.name}  changeJob={this.changeJob} job={this.state.job}  tituloRellena={this.state.tituloRellena} titleD={this.state.titleD} footerCopy={this.state.copyright} footerUrl={this.state.adalab} fontTypes={this.state.fontTypes} skills={this.state.habilidades} palette={this.state.palette} changePalette={this.changePalette} tipography={this.state.tipography} changeTipography={this.changeTipography} email={this.state.email} changeEmail={this.changeEmail} phone={this.state.phone} changePhone={this.changePhone} linkedin={this.state.linkedin} changeLinkedin={this.changeLinkedin} github={this.state.github} changeGithub={this.changeGithub} />
       </React.Fragment>
     );
   }
