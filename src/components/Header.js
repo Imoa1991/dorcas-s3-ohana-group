3183b40b08dd0580dc3d6ac235d368617104411c
andre <andreina.vargas@gmail.com>
2018-08-06 20:46:02 +0200

    hecho router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
 class Header extends React.Component {
 	render() {
 		return (
 			<header>
 				<div className="header__logo">
-					<img className="header__logo--image" src={this.props.logoCards} alt=""/>
+					<Link to='/'>
+						<img className="header__logo--image" src={this.props.logoCards} alt=""/>
+					</Link>
+
 				</div>
 			</header>
 			);
