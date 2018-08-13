1d646c3b9edb4487e1f05a0856ded20a554ba8b0
АНЯ <annya.branco@gmail.com>
2018-08-13 21:02:00 +0200

    Deleted old docs

--- src/components/Result.js
+++ src/components/Result.js
@@ -28,28 +28,28 @@ class Result extends React.Component {
     return (
       <div className="result__screen">
 
-          {this.props.generateCardClicked && this.props.finalCardToShare.cardURL === undefined && (
-            <div className="result__wait">
-              <img src={Loading} alt="Cargando resultado" className="result__wait--img"/>
-              <p className="result__wait--txt">La solicitud puede tardar algunos instantes.</p>
-            </div>
-          )}
-
-          {this.props.finalCardToShare.cardURL !== undefined && (
-            <section className="shares">
-              <p className="shares__text">La tarjeta ha sido creada:</p>
-
-              <a href={this.props.finalCardToShare.cardURL}  className="shares__url">
-<p>{this.props.finalCardToShare.cardURL}</p>
-              </a>
-
-              <a className="shares__twitter" type="button" name="button" target="_blank" href={`https://twitter.com/intent/tweet?url='${this.props.finalCardToShare.cardURL}'&text=My Awesome Profile-Card: ${this.props.finalCardToShare.cardURL} (powered by Ohana Group / @AdaLab_Digital) &hashtags=WomenInTech,Adalabers`}>
-                <i className="fab fa-twitter shares__twitter-logo"></i>
-                <p className="shares__twitter-text">Compartir en Twitter</p>
-              </a>
-
-          </section>
+        {this.props.generateCardClicked && this.props.finalCardToShare.cardURL === undefined && (
+          <div className="result__wait">
+            <img src={Loading} alt="Cargando resultado" className="result__wait--img"/>
+            <p className="result__wait--txt">La solicitud puede tardar algunos instantes.</p>
+          </div>
         )}
+
+        {this.props.finalCardToShare.cardURL !== undefined && (
+          <section className="shares">
+            <p className="shares__text">La tarjeta ha sido creada:</p>
+
+            <a href={this.props.finalCardToShare.cardURL}  className="shares__url" target="_blank">
+              <p>{this.props.finalCardToShare.cardURL}</p>
+            </a>
+
+            <a className="shares__twitter" type="button" name="button" target="_blank" href={`https://twitter.com/intent/tweet?url='${this.props.finalCardToShare.cardURL}'&text=My Awesome Profile-Card: ${this.props.finalCardToShare.cardURL} (powered by Ohana Group / @AdaLab_Digital) &hashtags=WomenInTech,Adalabers`}>
+            <i className="fab fa-twitter shares__twitter-logo"></i>
+            <p className="shares__twitter-text">Compartir en Twitter</p>
+          </a>
+
+        </section>
+      )}
     </div>
   );
 }
